34b1e73af75ba75cf7a6f336f62bb31af545141c
vin22222 <147446431+vin22222@users.noreply.github.com>
2023-10-14 16:57:28 +1300

    Add A Second Text Entry Box?

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,6 @@ my_data_rows = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 streamlit.header("The fruit_load_list contains")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thank you for adding ', add_my_fruit)
