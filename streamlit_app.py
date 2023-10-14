9a14471c8b102de9d7199b484f56c13ef3657dc1
vin22222 <147446431+vin22222@users.noreply.github.com>
2023-10-14 17:22:45 +1300

    Move the Fruityvice Code into a Try-Except (with a nested If-Else)

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,19 +25,21 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+try: 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.write('The user entered ', fruit_choice)
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response.json())
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+    # Take the json file and normalize it 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# Take the json file and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# Output it the screen as table
-streamlit.dataframe(fruityvice_normalized)
-
+    # Output it the screen as table
+    streamlit.dataframe(fruityvice_normalized)
+Except URLError as e:
+streamlit.error()
 
 streamlit.stop()
 
