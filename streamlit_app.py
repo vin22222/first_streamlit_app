e8817ea92f7bcb7225712ad0856e28be1adc42b3
vin22222 <147446431+vin22222@users.noreply.github.com>
2023-10-15 16:11:21 +1300

    Now Let's Use a Function and Button to Add the Fruit Name Submissions

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,6 +58,14 @@ if streamlit.button('Get Fruit Load List'):
      my_data_rows = get_fruit_load_list()
      streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thank you for adding ', add_my_fruit)
-my_cur.execute("insert into FRUIT_LOAD_LIST values ('from Streamlit')")
+streamlit.stop()
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into FRUIT_LOAD_LIST values ('from Streamlit')")
+    return "Thanks for adding " + new_fruit
+    
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
